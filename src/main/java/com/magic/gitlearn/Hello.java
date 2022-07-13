919aa266083932d006e5c37896da73dc86571a26
magic <77189117@qq.com>
2022-07-13 15:47:26 +0800

    add ssh

--- src/main/java/com/magic/gitlearn/Hello.java
+++ src/main/java/com/magic/gitlearn/Hello.java
@@ -3,5 +3,6 @@ package com.magic.gitlearn;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("SSH add");
     }
 }
